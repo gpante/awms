6191047fd2ab2a6107bfdab77e08083fc849b4e3
Ryan Morehouse <morehouse2@wisc.edu>
2019-10-29 13:59:17 -0500

    templates and updated pom.xml

--- src/main/java/com/cs506/HomePage.java
+++ src/main/java/com/cs506/HomePage.java
@@ -1,14 +1,15 @@
 package com.cs506;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import com.cs506.templates.Base;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.WebPage;
 
-public class HomePage extends WebPage {
+
+public class HomePage extends Base {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
-		super(parameters);
+		super();
 
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
