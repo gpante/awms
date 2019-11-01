54d1f07d30875ea459720f67d79d8d47426469d6
Ryan Morehouse <morehouse2@wisc.edu>
2019-10-31 23:31:56 -0500

    about pages

---
+++ src/main/java/com/cs506/AboutWorkshops.java
@@ -0,0 +1,27 @@
+
+
+
+package com.cs506;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import com.cs506.templates.Base;
+import org.apache.wicket.markup.html.basic.Label;
+
+
+public class AboutWorkshops extends Base {
+	private static final long serialVersionUID = 1L;
+
+	public AboutWorkshops(final PageParameters parameters) {
+		super();
+
+		//add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+
+		// TODO Add your page's components here
+
+	}
+}
+
+
+
+
+
