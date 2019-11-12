c360a44263ba11953e389ea64af7bdd5fbba14e7
Ryan Morehouse <morehouse2@wisc.edu>
2019-11-12 11:42:29 -0600

    small changes, started workshop table

---
+++ src/main/java/com/cs506/WorkshopTable.java
@@ -0,0 +1,17 @@
+package com.cs506;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import com.cs506.templates.Base;
+
+
+public class WorkshopTable extends Base {
+	private static final long serialVersionUID = 1L;
+
+	public WorkshopTable(final PageParameters parameters) {
+		super();
+
+		
+		
+
+	}
+}
